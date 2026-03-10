49f7bb9de499968d5a3cb4f0df2941f2742c6ad6
kairavsharma5 <kairavsharma452@gmail.com>
2026-03-10 13:25:39 +0530

    feat: Completed home page & added wipe functionality

--- app/components/ResumeCard.tsx
+++ app/components/ResumeCard.tsx
@@ -1,11 +1,27 @@
 import { Link } from "react-router";
 import ScoreCircle from "~/components/ScoreCircle";
+import { useEffect, useState } from "react";
+import { usePuterStore } from "~/lib/puter";
 
 const ResumeCard = ({
   resume: { id, companyName, jobTitle, feedback, imagePath },
 }: {
   resume: Resume;
 }) => {
+  const { fs } = usePuterStore();
+  const [resumeUrl, setResumeUrl] = useState("");
+
+  useEffect(() => {
+    const loadResume = async () => {
+      const blob = await fs.read(imagePath);
+      if (!blob) return;
+      let url = URL.createObjectURL(blob);
+      setResumeUrl(url);
+    };
+
+    loadResume();
+  }, [imagePath]);
+
   return (
     <Link
       to={`/resume/${id}`}
@@ -14,14 +30,10 @@ const ResumeCard = ({
       <div className="resume-card-header">
         <div className="flex flex-col gap-2">
           {companyName && (
-            <h2 className="text-black! font-bold wrap-break-word">
-              {companyName}
-            </h2>
+            <h2 className="text-black! font-bold wrap-break-word">{companyName}</h2>
           )}
           {jobTitle && (
-            <h3 className="text-lg wrap-break-word text-gray-500">
-              {jobTitle}
-            </h3>
+            <h3 className="text-lg wrap-break-word text-gray-500">{jobTitle}</h3>
           )}
           {!companyName && !jobTitle && (
             <h2 className="text-black! font-bold">Resume</h2>
@@ -31,11 +43,11 @@ const ResumeCard = ({
           <ScoreCircle score={feedback.overallScore} />
         </div>
       </div>
-      {imagePath && (
+      {resumeUrl && (
         <div className="gradient-border animate-in fade-in duration-1000">
           <div className="w-full h-full">
             <img
-              src={imagePath}
+              src={resumeUrl}
               alt="resume"
               className="w-full h-[350px] max-sm:h-[200px] object-cover object-top"
             />
